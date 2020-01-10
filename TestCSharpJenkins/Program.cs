efb0492b3f46249e858aea6ef8c5488a63d63aec
Hoang Minh Dung (RBVH/EDA34) <uoa1hc@bosch.com>
2020-01-10 15:43:06 +0700

    Add project files.

---
+++ TestCSharpJenkins/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestCSharpJenkins
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is C# application on Jenkins!");
+            Console.ReadLine();
+        }
+    }
+}
